e90c0b40a94395b5de147d880ccdb263667d576f
boyxgz <boyxgz@msn.com>
2016-01-30 13:16:48 +0800

    config fix

--- vehicle-management/grails-app/conf/Config.groovy
+++ vehicle-management/grails-app/conf/Config.groovy
@@ -14,6 +14,7 @@
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
+grails.app.context = "/"
 grails.plugin.springsecurity.logout.postOnly = false
 grails.mime.types = [
     all:           '*/*',
